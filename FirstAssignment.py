a95297f0868b00428de76c3979a11bd3e7256aef
callistaminique09 <76256230+callistaminique09@users.noreply.github.com>
2021-09-25 12:50:33 +0700

    Create FirstAssignment.py

---
+++ FirstAssignment.py
@@ -0,0 +1,9 @@
+#First Assignment  
+nama ="Callista Dominique" #string
+umur = 20 #Integer
+tinggal = "Cikarang" #string
+
+#soal/question
+print("Hai namaku adalah {}, umurku {}, sekarang tinggal di {}".format(nama,umur,tinggal))
+
+#Hasil/Result= "Hai namaku adalah Callista Dominique, umurku 20, sekarang tinggal di Cikarang"
