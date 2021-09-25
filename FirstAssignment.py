130ddea7c71304d2dff0cbdc723fec5bb53bb1dc
callistaminique09 <76256230+callistaminique09@users.noreply.github.com>
2021-09-25 12:54:20 +0700

    Update FirstAssignment.py

--- FirstAssignment.py
+++ FirstAssignment.py
@@ -6,4 +6,4 @@ tinggal = "Cikarang" #string
 #soal/question
 print("Hai namaku adalah {}, umurku {}, sekarang tinggal di {}".format(nama,umur,tinggal))
 
-#Hasil/Result= "Hai namaku adalah Callista Dominique, umurku 20, sekarang tinggal di Cikarang"
+#Hasil/Result= "Hai namaku adalah Callista Dominique, umurku 20, sekarang tinggal di Cikarang".
